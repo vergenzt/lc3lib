044ed1b06c4699a1b20d394784819523a9a8a440
Tim Vergenz <vergenzt@gmail.com>
2012-12-08 19:54:39 -0500

    Add initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='lc3lib',
+    url='http://www.github.com/vergenzt/lc3lib',
+    version='0.1',
+    packages=['lc3lib'],
+    package_dir={'lc3lib':'src/lc3lib'}
+)
